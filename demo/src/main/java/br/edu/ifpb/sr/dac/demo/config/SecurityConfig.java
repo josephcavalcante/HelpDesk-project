0aa43b5457418feed053cac20e41a83de3d0be73
Joseph Nichollas <nichollascavalcante@gmail.com>
2026-02-26 09:46:42 -0300

    feat: configura Spring Security para ignorar CSRF e libera acesso a endpoints de /v1/usuarios

--- demo/src/main/java/br/edu/ifpb/sr/dac/demo/config/SecurityConfig.java
+++ demo/src/main/java/br/edu/ifpb/sr/dac/demo/config/SecurityConfig.java
@@ -2,13 +2,29 @@ package br.edu.ifpb.sr.dac.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v1/usuarios/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
