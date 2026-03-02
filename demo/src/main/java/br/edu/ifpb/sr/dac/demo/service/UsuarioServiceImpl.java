b7497f11ebbe40c4423e62739688a7167940b743
Joseph Nichollas <nichollascavalcante@gmail.com>
2026-03-02 16:59:19 -0300

    feat: Adiciona o MapStruct.

--- demo/src/main/java/br/edu/ifpb/sr/dac/demo/service/UsuarioServiceImpl.java
+++ demo/src/main/java/br/edu/ifpb/sr/dac/demo/service/UsuarioServiceImpl.java
@@ -3,9 +3,9 @@ package br.edu.ifpb.sr.dac.demo.service;
 import br.edu.ifpb.sr.dac.demo.dao.UsuarioDao;
 import br.edu.ifpb.sr.dac.demo.dto.GetUsuariosDTO;
 import br.edu.ifpb.sr.dac.demo.dto.PostUsuarioDTO;
+import br.edu.ifpb.sr.dac.demo.dto.UsuarioMapper;
 import br.edu.ifpb.sr.dac.demo.model.Usuario;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,25 +16,25 @@ import java.util.stream.Collectors;
 public class UsuarioServiceImpl implements UsuarioService {
     private final PasswordEncoder passwordEncoder;
     private final UsuarioDao usuarioDao;
-    public UsuarioServiceImpl (UsuarioDao usuarioDao, PasswordEncoder passwordEncoder){
+    private final UsuarioMapper usuarioMapper;
+    public UsuarioServiceImpl (UsuarioDao usuarioDao, PasswordEncoder passwordEncoder, UsuarioMapper usuarioMapper){
         this.usuarioDao = usuarioDao;
         this.passwordEncoder = passwordEncoder;
+        this.usuarioMapper = usuarioMapper;
     }
 
     @Override
     @Transactional
     public void save(PostUsuarioDTO dto) {
-        Usuario usuario = new Usuario();
-        usuario.setNome(dto.nome());
-        usuario.setSenha(passwordEncoder.encode(dto.senha()));
-        usuario.setUsername(dto.username());
+        Usuario usuario = this.usuarioMapper.toUsuarioEntity(dto);
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
         this.usuarioDao.save(usuario);
     }
 
     @Override
     public List<GetUsuariosDTO> findAll() {
-        return this.usuarioDao.findAll().stream()
-                .map(usuario -> new GetUsuariosDTO(usuario.getId(), usuario.getNome(), usuario.getUsername()))
-                .collect(Collectors.toList());
+        return this.usuarioDao.findAll().
+                stream().
+                map(this.usuarioMapper::toDto).toList();
     }
 }
