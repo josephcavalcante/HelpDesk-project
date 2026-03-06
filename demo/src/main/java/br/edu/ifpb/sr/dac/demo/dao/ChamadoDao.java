c930a1e65048c952c9f7da53fd3492e8d0624380
Joseph Nichollas <nichollascavalcante@gmail.com>
2026-03-06 14:29:24 -0300

    feat: adiciona chamadoDao

--- demo/src/main/java/br/edu/ifpb/sr/dac/demo/dao/ChamadoDao.java
+++ demo/src/main/java/br/edu/ifpb/sr/dac/demo/dao/ChamadoDao.java
@@ -4,6 +4,13 @@ import br.edu.ifpb.sr.dac.demo.model.Chamado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChamadoDao  extends JpaRepository<Chamado, Long> {
+
+    List<Chamado> findByUsuarioId(Long usuarioId);
+    List<Chamado> finbByStatus(String status);
+    List<Chamado> findByUsuarioIdAndStatus(Long usuarioId, String status);
+
 }
